5f6074afd4a8071334d57e6f086c790bc00c38ad
VictorDev <ememevictor08@gmail.com>
2025-06-26 16:11:52 +0100

    First migration commit

---
+++ db/models/user.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model,
+  Sequelize
+} = require('sequelize');
+const sequelize = require('../../config/database');
+module.exports = sequelize.define('user', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER
+  },
+  username: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  createdAt: {
+    allowNull: false,
+    type: Sequelize.DATE
+  },
+  updatedAt: {
+    allowNull: false,
+    type: Sequelize.DATE
+  },
+  deletedAt: {
+    type: Sequelize.DATE
+  }
+}, {
+  paranoid: true, // Enable soft deletes
+  freezeTableName: true,
+  modelName: 'user',
+})
